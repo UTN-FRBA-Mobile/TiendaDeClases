6654cab0d61addf7f9c5eca1fe6a974665d02fb3
Christian Coello Grassi <christian_coello_g@hotmail.com>
2018-09-24 16:41:44 -0300

    Transicion al tutorial despues de 3 segundos y tutoriales base, solo texto

--- TiendaDeClases/app/src/main/java/desarrollomobile/tiendadeclases/tiendadeclases/MainActivity.kt
+++ TiendaDeClases/app/src/main/java/desarrollomobile/tiendadeclases/tiendadeclases/MainActivity.kt
@@ -2,11 +2,17 @@ package desarrollomobile.tiendadeclases.tiendadeclases
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_splash)
+        Handler().postDelayed(
+                {
+                    setContentView(R.layout.fragment_tutorial1)
+                },3500 )
     }
 }
