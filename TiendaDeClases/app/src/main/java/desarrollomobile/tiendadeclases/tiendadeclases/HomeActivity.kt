f37d73fbc4bb6e9670713f5a0f5972e774af3e26
aguspina <aguspina87@gmail.com>
2018-10-01 20:22:16 -0300

    Add 3 home fragments

--- TiendaDeClases/app/src/main/java/desarrollomobile/tiendadeclases/tiendadeclases/HomeActivity.kt
+++ TiendaDeClases/app/src/main/java/desarrollomobile/tiendadeclases/tiendadeclases/HomeActivity.kt
@@ -1,23 +1,27 @@
 package desarrollomobile.tiendadeclases.tiendadeclases
 
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), CategoriesFragment.OnFragmentInteractionListener, ClassesFragment.OnFragmentInteractionListener, MessagesFragment.OnFragmentInteractionListener {
+    override fun onFragmentInteraction(uri: Uri) {
+
+    }
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+            R.id.navigation_categories -> {
+                supportFragmentManager.beginTransaction().replace(R.id.HomeFrame, CategoriesFragment()).commit()
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+            R.id.navigation_classes -> {
+                supportFragmentManager.beginTransaction().replace(R.id.HomeFrame, ClassesFragment()).commit()
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+            R.id.navigation_messages -> {
+                supportFragmentManager.beginTransaction().replace(R.id.HomeFrame, MessagesFragment()).commit()
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -27,6 +31,7 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        var navigation = findViewById<BottomNavigationView>(R.id.navigation)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
--- TiendaDeClases/app/src/main/java/desarrollomobile/tiendadeclases/tiendadeclases/MainActivity.kt
+++ TiendaDeClases/app/src/main/java/desarrollomobile/tiendadeclases/tiendadeclases/MainActivity.kt
@@ -1,11 +1,12 @@
 package desarrollomobile.tiendadeclases.tiendadeclases
 
 import android.net.Uri
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentTransaction
+import android.support.v7.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity(), SplashFragment.OnFragmentInteractionListener, Tutorial1Fragment.OnFragmentInteractionListener, LoginFragment.OnFragmentInteractionListener {
     override fun onFragmentInteraction(uri: Uri) {
@@ -20,7 +21,7 @@ class MainActivity : AppCompatActivity(), SplashFragment.OnFragmentInteractionLi
         Handler().postDelayed(
                 {
                     showAnimatedFragment(Tutorial1Fragment())
-                },3500 )
+                }, 3500)
     }
 
     fun showAnimatedFragment(fragment: Fragment) {
@@ -30,6 +31,5 @@ class MainActivity : AppCompatActivity(), SplashFragment.OnFragmentInteractionLi
                 .replace(R.id.container, fragment)
                 .commit()
     }
-
 
 }
