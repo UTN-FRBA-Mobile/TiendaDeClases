9a46ea5ef25cd8808aa9288d9c92bb0c92395520
Leandro Carbajales <lcarbajales@C02Q322AG8WM.local>
2018-10-01 18:06:03 -0300

    Pantalla login y boton en tutorial para ir al login

--- TiendaDeClases/app/src/main/java/desarrollomobile/tiendadeclases/tiendadeclases/MainActivity.kt
+++ TiendaDeClases/app/src/main/java/desarrollomobile/tiendadeclases/tiendadeclases/MainActivity.kt
@@ -3,6 +3,8 @@ package desarrollomobile.tiendadeclases.tiendadeclases
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.widget.Button
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,14 @@ class MainActivity : AppCompatActivity() {
         Handler().postDelayed(
                 {
                     setContentView(R.layout.fragment_tutorial1)
+
+                    val button = findViewById<Button>(R.id.Tutorial4_button)
+                    button.setOnClickListener{
+                        setContentView(R.layout.fragment_fragment_login)
+                    }
                 },3500 )
     }
+
+
+
 }
