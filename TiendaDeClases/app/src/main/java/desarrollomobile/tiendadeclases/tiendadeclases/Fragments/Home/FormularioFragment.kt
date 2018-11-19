33ab42f6b9397ca0f6df515efe6dda3917781ebc
yamilaBacchini <yamila.bacchini@gmail.com>
2018-11-19 15:21:24 -0300

    agrego que tome la localizacion del dispositivo en el formulario

--- TiendaDeClases/app/src/main/java/desarrollomobile/tiendadeclases/tiendadeclases/Fragments/Home/FormularioFragment.kt
+++ TiendaDeClases/app/src/main/java/desarrollomobile/tiendadeclases/tiendadeclases/Fragments/Home/FormularioFragment.kt
@@ -1,14 +1,22 @@
 package desarrollomobile.tiendadeclases.tiendadeclases.Fragments.Home
 
 import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.net.Uri
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import desarrollomobile.tiendadeclases.tiendadeclases.R
-
+import kotlinx.android.synthetic.main.fragment_formulario.*
+import kotlin.properties.Delegates
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,11 +32,20 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  *
  */
-class FormularioFragment : Fragment() {
+class FormularioFragment : Fragment(){
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private var listener: OnFragmentInteractionListener? = null
+    private var listener: CategoriesFragment.OnFragmentInteractionListener? = null
+
+    private var locationManager : LocationManager? = null
+    var deschequear: Boolean by Delegates.observable(false) {
+        prop, old, new ->
+            if(new) {
+                cbUbicacionActual.isChecked = false
+                deschequear = false
+            }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,18 +61,57 @@ class FormularioFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_formulario, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        cbUbicacionActual.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked){
+                try {
+                    if (ActivityCompat.checkSelfPermission(context!!,
+                                    android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context!!,
+                                    android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        requestPermissions(
+                                arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION),
+                                100)
+                    } else {
+                        locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0L, 0f, locationListener);
+                        Log.e("DB", "PERMISSION GRANTED")
+                    }
+                } catch(ex: SecurityException) {
+                    Log.d("myTag", "Security Exception, no location available");
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        try {
+            if(grantResults.all { x -> x == PackageManager.PERMISSION_GRANTED}){
+                locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0L, 0f, locationListener);
+            }else{
+                Toast.makeText(contexto, "No va a poder utilizar la funcion de localizar automaticamente sin el permiso de GPS aceptado", Toast.LENGTH_SHORT).show()
+                deschequear = true
+            }
+        } catch(ex: SecurityException) {
+            Log.d("myTag", "Security Exception, no location available");
+        }
+    }
+
     // TODO: Rename method, update argument and hook method into UI event
     fun onButtonPressed(uri: Uri) {
         listener?.onFragmentInteraction(uri)
     }
 
+    var contexto : Context ?= null
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
+        if (context is CategoriesFragment.OnFragmentInteractionListener) {
             listener = context
         } else {
             throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
         }
+        locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager?;
+        contexto = context
     }
 
     override fun onDetach() {
@@ -97,5 +153,21 @@ class FormularioFragment : Fragment() {
                         putString(ARG_PARAM2, param2)
                     }
                 }
+    }
+
+    var latitude : Double ?= null
+    var longitude : Double ?= null
+    private val locationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            latitude = location.latitude
+            longitude = location.longitude
+            //Toast.makeText(contexto, "La ubicacion es la siguiente: latitud -> " + latitude + " longitude -> " + longitude, Toast.LENGTH_SHORT).show()
+        }
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {
+            deschequear = true
+            Toast.makeText(contexto, "El GPS se debe encontrar encendido para poder utilzar la ubicacion actual", Toast.LENGTH_SHORT).show()
+        }
     }
 }
